1567ea8d30de47a9c7afcec691ade863bcee45cc
andrewvt100 <andrewvt100@gmail.com>
2016-11-04 10:04:07 -0700

    XCode Project Initialized

---
+++ swift-algorithms-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-algorithms-ios
+//
+//  Created by Andrew van Tonningen on 11/4/16.
+//  Copyright © 2016 Andrew van Tonningen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
